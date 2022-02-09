ecc20224bb3e8594cb395711972c8512b2ff5bd7
vasily_byakhov <vasily.byakhov@sravni.ru>
2022-02-09 23:40:13 +0300

    Добавили файл билдгрейдл

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,35 @@
+package junit;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+///класс называем по общему компоненту который тестируем
+public class FirstJUnitTest {
+
+    ///Анотация которая будет выполнена перед тестом
+    @BeforeEach
+    void  before() {
+        System.out.println("Этот метод выполняется перед каждым тестом");
+    }
+
+    ///Анотация что нужно сделать после теста
+    @AfterEach
+    void after() {
+        System.out.println("Этот метод выполняется перед каждым тестом");
+    }
+
+    @Test
+    ///Метод который выполняет проверку по условиям. Методы называем исходя из отдельного функционала который проверяем.
+    void textTest() {
+        //поиск по тексту
+        System.out.println("Это первый тест");
+    }
+
+    @Test
+    void  videoTest (){
+        //поиск видео
+        System.out.println("Это второй тест");
+    }
+}
