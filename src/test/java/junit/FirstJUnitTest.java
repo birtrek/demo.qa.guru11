89a9bf8daf2a47db78ca315c7f277baede418d8a
vasily_byakhov <vasily.byakhov@sravni.ru>
2022-02-10 10:45:11 +0300

    Добавлены тесты с анотациями

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 ///класс называем по общему компоненту который тестируем
 public class FirstJUnitTest {
 
