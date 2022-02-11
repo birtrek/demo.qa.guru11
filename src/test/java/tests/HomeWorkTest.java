93d06f581e8699558bdebd22d77fb421a1abea4d
vasily_byakhov <vasily.byakhov@sravni.ru>
2022-02-11 13:08:24 +0300

    Добавлена проверка по полям

--- src/test/java/tests/HomeWorkTest.java
+++ src/test/java/tests/HomeWorkTest.java
@@ -28,7 +28,7 @@ public class HomeWorkTest {
         $("#lastName").setValue("Olegovich");
         $("#userEmail").setValue("Vova@mail.ru");
         $(byText("Male")).click();
-        $("#userNumber").setValue("71234567896");
+        $("#userNumber").setValue("7123456789");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July"); // я хотел очистить поле календаря и подставить заготовленную маску 10 Feb 2022
         $(".react-datepicker__year-select").selectOption("2010");
@@ -44,6 +44,18 @@ public class HomeWorkTest {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text("Student Name " + "Vova" + " " + "Olegovich"),
+                text("Student Email " + "Vova@mail.ru"),
+                text("Gender " + "Male"),
+                text("Mobile " + "7123456789"),
+                text("Date of Birth " + "06 July,2010"),
+                text("Subjects " + "Biology"),
+                text("Hobbies " + "Sports"),
+                text("Picture " + "123.jpg"),
+                text("Address " + "Moscow"),
+                text("State and City " + "Haryana" + " " + "Panipat")
+                );
 
 
     }
